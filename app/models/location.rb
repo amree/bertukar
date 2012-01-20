10d7d4faeda3ec63fef7ea6f0c6e08fd4d441b0c
amree <amree@ieatbinary.com>
2012-01-20 15:51:14 +0800

    Added Location's relationship and tests for them

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ActiveRecord::Base
+  belongs_to :state, class_name: "Location", foreign_key: :state_id
+  has_many :districts, class_name: "Location", foreign_key: :state_id
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Location do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @location = Factory.build(:location)
+  end
+
+  describe "relationship" do
+    before do
+      @location.save
+
+      @location.districts.build
+      @location.districts[0] = Factory.build(:location)
+      @location.save
+    end
+
+    it "state should save districts correctly" do
+      @location.districts.count.should eql(1)
+    end
+
+    it "district should get state correctly" do
+      district = @location.districts.first
+
+      district.state.should_not be_nil
+    end
+  end
 end
