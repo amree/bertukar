0c541dd70a2b06420a68e2d6ea30f10edd4e6bee
amree <amree@ieatbinary.com>
2012-01-20 21:27:10 +0800

    Updated seeds to fill in state and district in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+states = Array.new
+states << { "nama" => "Kuala Lumpur" }
+states << { "nama" => "Labuan"}
+states << { "nama" => "Putrajaya"}
+states << { "nama" => "Johor",           "districts" => ["Johor Bahru", "Batu Pahat", "Muar", "Kluang", "Kota Tinggi", "Segamat", "Pontian", "Mersing", "Kulaijaya", "Ledang"]}
+states << { "nama" => "Kedah",           "districts" => ["Baling" ,"Bandar" ,"Baharu" ,"Kota Setar" ,"Kuala" ,"Muda" ,"Kubang" ,"Pasu" ,"Kulim" ,"Langkawi" ,"Padang" ,"Terap" ,"Pendang" ,"Pokok" ,"Sena" ,"Sik" ,"Yan"]}
+states << { "nama" => "Kelantan",        "districts" => ["Kota Bharu", "Pasir Mas", "Tumpat", "Pasir Puteh", "Bachok", "Kuala Krai", "Machang", "Tanah Merah", "Jeli", "Gua", "Musang"]}
+states << { "nama" => "Melaka",          "districts" => ["Alor Gajah", "Melaka Tengah", "Jasin"]}
+states << { "nama" => "Negeri Sembilan", "districts" => ["Jelebu", "Jempol", "Kuala Pilah", "Port Dickson", "Rembau", "Seremban", "Tampin"]}
+states << { "nama" => "Pahang",          "districts" => ["Bera", "Bentong", "Cameron Highlands", "Jerantut", "Kuantan", "Lipis", "Maran", "Pekan", "Raub", "Rompin", "Temerloh"]}
+states << { "nama" => "Perak",           "districts" => ["Kinta", "Larut, Matang dan Selama", "Hilir Perak", "Manjung", "Batang Padang", "Kerian", "Kuala Kangsar", "Hulu Perak", "Perak Tengah", "Kampar"]}
+states << { "nama" => "Perlis",          "districts" => ["Abi", "Arau", "Beseri", "Chuping", "Jejawi", "Kayang", "Kechor", "Kuala Perlis", "Kurong Anai", "Kurong Batang", "Ngulang", "Oran", "Padang Pauh", "Paya", "Pedang Siding", "Sanglang", "Sena", "Seriab", "Sungai Adam", "Sungai Baru", "Titi Tinggi", "Utan Aji", "Wang Bintong"]}
+states << { "nama" => "Pulau Pinang",    "districts" => ["Barat Daya Pulau Pinang", "Timur Laut Pulau Pinang", "Seberang Perai Utara", "Seberang Perai Tengah", "Seberang Perai Selatan"]}
+states << { "nama" => "Sabah",           "districts" => ["Pedalaman", "Kudat", "Sandakan", "Tawau", "Pantai Barat"]}
+states << { "nama" => "Sarawak",         "districts" => ["Betong", "Bintulu", "Kapit", "Kuching", "Limbang", "Miri", "Mukah", "Samarahan", "Sarikei", "Sibu", "Sri Aman"]}
+states << { "nama" => "Selangor",        "districts" => ["Gombak", "Hulu Langat", "Hulu Selangor", "Klang", "Kuala Langat", "Kuala Selangor", "Sabak Bernam", "Sepang", "Petaling"]}
+states << { "nama" => "Terengganu",      "districts" => ["Kuala Terengganu", "Hulu Terengganu", "Besut", "Setiu", "Marang", "Dungun", "Kemaman"]}
+
+ActiveRecord::Base.transaction do
+  states.each do |state|
+    s = Location.new(nama: state["nama"])
+    s.save!
+
+    unless state["districts"].nil?
+      state["districts"].each_with_index do |district, i|
+        d = Location.new(nama: district, state: s)
+        d.save!
+      end
+    end # unless
+  end # states
+end
