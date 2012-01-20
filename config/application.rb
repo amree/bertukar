48967887a2533019a26b929a1976068069333f92
amree <amree@ieatbinary.com>
2012-01-20 09:34:26 +0800

    Make sure factory_girl fixture's is generated by default

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,7 @@ module Pertukaran
         :view_specs    => false,
         :request_specs => false,
         :routing_specs => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
     end
   end
 end
