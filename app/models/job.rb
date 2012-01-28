34e710aa2d6fbe819bc7ac50a4cee079bfa08f41
amree <aurorius@gmail.com>
2012-01-28 10:38:14 +0800

    Added jenis tawaran jawatan

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,6 @@
 class Job < ActiveRecord::Base
   belongs_to :user
   belongs_to :location
+
+  JENIS = ["KOSONG", "SUKA SAMA SUKA"]
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have 2 jenis" do
+    Job::JENIS.count.should eq(2)
+  end
 end
