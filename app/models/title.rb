063a64aad120291657aa3574b69f77c4db97c11d
amree <aurorius@gmail.com>
2012-01-28 23:20:50 +0800

    Added some validations to Title

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,2 +1,7 @@
 class Title < ActiveRecord::Base
+  validates :nama, uniqueness: true, presence: true
+  validates :skim, presence: true
+  validates :skim, :format => { :with => /\A[a-zA-Z]+\z/, :message => "Only letters allowed" }
+
+  auto_strip_attributes :nama, :skim, squish: true
 end
--- spec/models/title_spec.rb
+++ spec/models/title_spec.rb
@@ -1,4 +1,38 @@
 require 'spec_helper'
 
 describe Title do
+  before do
+    @title = Factory.build(:title)
+  end
+
+  describe "validations" do
+    describe "nama" do
+      it "should not be nil" do
+        @title.nama = nil
+
+        @title.should_not be_valid
+      end
+
+      it "should be unique" do
+        @title.save
+        title2 = Factory.build(:title, nama: @title.nama)
+
+        title2.should_not be_valid
+      end
+    end
+
+    describe "skim" do
+      it "should not be nil" do
+        @title.skim = nil
+
+        @title.should_not be_valid
+      end
+
+      it "should only be letters" do
+        @title.skim = "12abc"
+
+        @title.should_not be_valid
+      end
+    end
+  end
 end
