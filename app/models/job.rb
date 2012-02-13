e7dec654666021a3e963cd33c6ecdd09a106d23c
amree <aurorius@gmail.com>
2012-02-13 23:09:09 +0800

    Get simple adding new Job for tawaran to work

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,7 @@
 class Job < ActiveRecord::Base
+  has_many   :next_jobs, class_name: "Job"
+  belongs_to :current_job, class_name: "Job"
   belongs_to :user
   belongs_to :location
-
-  JENIS = ["KOSONG", "SUKA SAMA SUKA"]
+  belongs_to :title
 end
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
 describe Job do
-  it "should have 2 jenis" do
-    Job::JENIS.count.should eq(2)
-  end
+
 end
