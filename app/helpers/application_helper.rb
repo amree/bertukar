ea457c10b542478d0147cb549cf2b0e3e08bd6f8
amree <aurorius@gmail.com>
2012-02-20 22:25:47 +0800

    Added helper to show notification message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  # TODO: Test show_flash
+  def show_flash(notice, alert)
+    type = notice.blank? ? "error" : "success"
+
+    html = ""
+    unless notice.blank? and alert.blank?
+      html += content_tag :div, id: "flash", class: "alert alert-#{type} fade in" do
+        content_tag(:a, raw("&times;"), class: "close", :"data-dismiss" => "alert", href: "#") +
+        (notice || alert)
+      end
+    end
+    raw html
+  end
 end
