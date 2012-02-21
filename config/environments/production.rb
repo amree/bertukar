3692fee87222ccfa7d32dea55fdcd52ddd72f682
amree <aurorius@gmail.com>
2012-02-22 00:02:50 +0800

    Added host's info for mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,7 @@ Pertukaran::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Devise
+  config.action_mailer.default_url_options = { :host => 'pertukaran.herokuapp.com' }
 end
