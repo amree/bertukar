5092ec0edbfcc31aaece5adacb4ce821a4584e10
amree <aurorius@gmail.com>
2012-07-08 08:21:58 +0800

    Converted Ministry's unit test to TestUnit

--- test/unit/ministry_test.rb
+++ test/unit/ministry_test.rb
@@ -1,11 +1,32 @@
 require 'test_helper'
 
 class MinistryTest < ActiveSupport::TestCase
+  setup do
+    @kkm = ministries(:kkm)
+  end
 
   test "should not be valid without nama" do
-    kkm = ministries(:kkm)
-    kkm.nama = nil
+    @kkm.nama = nil
 
-    assert !kkm.valid?
+    assert !@kkm.valid?
+  end
+
+  test "should not be valid without a unique name" do
+    kpt = ministries(:kpt)
+    kpt.nama = @kkm.nama
+
+    assert !kpt.valid?
+  end
+
+  test "should be active by default" do
+    assert @kkm.is_aktif
+  end
+
+  test "should have name in TitleCase" do
+    title = "Nama baru kkm"
+    @kkm.nama = title
+    @kkm.save
+
+    assert_equal @kkm.nama, title.titleize
   end
 end
