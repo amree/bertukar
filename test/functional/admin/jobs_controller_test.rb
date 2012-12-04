cbf9776cfca632ffb09a6c19e1f80a9f802ccf75
amree <aurorius@gmail.com>
2012-12-04 18:57:50 +0800

    Added functional tests Users and Job

---
+++ test/functional/admin/jobs_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class Admin::JobsControllerTest < ActionController::TestCase
+  def setup
+    @current = jobs(:current)
+  end
+
+  test "should get index" do
+    get :index
+
+    assert assigns(:jobs)
+    assert assigns(:jobs).include? @current
+  end
+
+  test "should get show" do
+    get :show, id: @current.to_param
+
+    assert_equal assigns(:job), @current
+  end
+end
---
+++ test/functional/admin/users_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class Admin::UsersControllerTest < ActionController::TestCase
+  def setup
+    @user = users(:user)
+  end
+
+  test "should get index" do
+    get :index
+
+    assert assigns(:users)
+    assert assigns(:users).include? @user
+  end
+
+  test "should get show" do
+    get :show, id: @user.to_param
+
+    assert_equal assigns(:user), @user
+  end
+end
