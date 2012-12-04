27a8f73a0fa7e1340964f6a4c684e782d873065a
amree <aurorius@gmail.com>
2012-12-04 19:33:27 +0800

    Totally removed RSpec tests

--- old_tests/factories/titles.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :title do
-    sequence(:nama) { |n| "#{Faker::Name.name} #{n}" }
-    sequence(:skim) { [*('A'..'Z')].sample(2).join }
-  end
-end
--- old_tests/factories/users.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :user do
-    sequence(:email)    { |n| "#{Faker::Internet.email} #{n}" }
-    sequence(:password) { |n| "#{Faker::Name.name} #{n}" }
-  end
-end
