37aa24623fe80e70ecf9c3a0a4b71431b2581000
amree <aurorius@gmail.com>
2012-12-05 22:51:48 +0800

    Deleting user should delete his/her job offers. Closes #12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :jobs
+  has_many :jobs, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:user)
+  end
+
+  test "deleting a user should delete its job" do
+    assert_difference("Job.count", -2) do
+      @user.destroy
+    end
+  end
 end
