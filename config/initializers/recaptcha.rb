c11b278611abc41a9878778b244de1ed55bf7b7c
amree <aurorius@gmail.com>
2012-12-07 07:06:39 +0800

    Removed some files from ignore list for Heroku

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LcDBdoSAAAAAPG7s8qnOUZQ6UGtQesW_9g0ZrN8'
+  config.private_key = '6LcDBdoSAAAAADaBdeZNYQM2CGBDwo6HErLD9rp-'
+  config.proxy = 'http://proxy.kk.usm.my:8080'
+end
