75f35bc699ea7c3fd80fceec9878b18e4e8685e7
amree <aurorius@gmail.com>
2012-12-15 11:41:05 +0800

    Updated email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,5 +59,16 @@ Bertukar::Application.configure do
   config.active_support.deprecation = :notify
 
   # Devise
-  config.action_mailer.default_url_options = { :host => 'pertukaran.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'bertukar.heroku.com' }
+
+  # Heroku Mailgun
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'bertukar.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
