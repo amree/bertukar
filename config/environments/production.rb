8e4fb26b74ca4c4694fa6572c2c43bfc5f55a560
amree <aurorius@gmail.com>
2012-12-15 12:39:42 +0800

    Use SendGrid instead of MailGun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,14 +61,14 @@ Bertukar::Application.configure do
   # Devise
   config.action_mailer.default_url_options = { :host => 'bertukar.heroku.com' }
 
-  # Heroku Mailgun
+  # Heroku Sendgrid
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'bertukar.heroku.com',
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
     :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
   }
   ActionMailer::Base.delivery_method = :smtp
 end
