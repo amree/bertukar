430fb4df5952aeeb1c686ce06098cba817c53f3f
amree <aurorius@gmail.com>
2012-12-28 21:10:37 +0800

    Added jobs listing to sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -4,6 +4,7 @@ class SitemapController < ApplicationController
   def index
     static_urls = [ {:url => '/about',      :updated_at => ""} ]
     @pages_to_visit  = static_urls
+    @pages_to_visit += Location.states.all.collect { |l| { url: listings_states_path(id: l.to_param), updated_at: l.updated_at.xmlschema } }
     @pages_to_visit += Job.main.all.collect { |a| { url: job_path(a), updated_at: a.updated_at.xmlschema } }
     respond_to do |format|
       format.xml
