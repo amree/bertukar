2a45dd75e04afbc4abbda27bdf41234841ce65ce
amree <aurorius@gmail.com>
2013-01-18 14:42:45 +0800

    Attempting to track new user's registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,8 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     if verify_recaptcha
       super
+      session[:events] ||= Array.new
+      session[:events] << { category: "user", action: "create", label: "count", value: User.count }
     else
       build_resource
       clean_up_passwords(resource)
