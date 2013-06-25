81f281956eaa580c43b67cfce327ffa607533baa
amree <aurorius@gmail.com>
2013-06-25 22:24:18 +0800

    Readded errbit config and use ENV for the value

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
-  config.api_key = ''
-  config.host    = ''
+  config.api_key = ENV['ERRBIT_KEY']
+  config.host    = 'errbitee.herokuapp.com'
   config.port    = 80
   config.secure  = config.port == 443
 end
